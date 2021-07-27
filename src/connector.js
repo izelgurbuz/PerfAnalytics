d5a01a5da00aa3155946c4af72568c7c23adad2b
Izel Gurbuz Culban <izelgurbuz@anayurtteknoloji.com>
2021-07-27 10:00:30 +0300

    temporary solution for db connection (no secret key)

--- src/connector.js
+++ src/connector.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   this.connect = function () {
     return new Promise((resolve, reject) => {
-      mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true });
+      mongoose.connect("mongodb+srv://izelgurbuz:1234izel@prefanalytics.d10oy.mongodb.net/PrefAnalytics?retryWrites=true&w=majority", { useNewUrlParser: true });
       mongoose.connection.on("error", (error) => {
         reject(error);
       });
