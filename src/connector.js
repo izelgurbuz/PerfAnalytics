63136bd1ebba45fb392c2ebf5c22f40a4403c3f2
Izel Gurbuz Culban <izelgurbuz@gmail.com>
2021-09-05 23:04:48 +0300

    Update connector.js

--- src/connector.js
+++ src/connector.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   this.connect = function () {
     return new Promise((resolve, reject) => {
-      mongoose.connect(${{ secrets.MONGO_URI }}, { useNewUrlParser: true });
+      mongoose.connect(process.env.MONGO_URI || "", { useNewUrlParser: true });
       mongoose.connection.on("error", (error) => {
         reject(error);
       });
